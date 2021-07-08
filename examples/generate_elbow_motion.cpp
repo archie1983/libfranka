dc6534e1ece66039fcb408204aa7c4542d47d4d4
Arturs Elksnis <arturs@elksnis.co.uk>
2021-07-08 13:11:05 +0100

    My first attempt at commanding elbow

--- examples/generate_elbow_motion.cpp
+++ examples/generate_elbow_motion.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
     // First move the robot to a suitable joint configuration
     std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
     MotionGenerator motion_generator(0.5, q_goal);
-    std::cout << "WARNING: This example will move the robot! "
+    std::cout << "WARNING AE: This example will move the robot! "
               << "Please make sure to have the user stop button at hand!" << std::endl
               << "Press Enter to continue..." << std::endl;
     std::cin.ignore();
@@ -67,6 +67,40 @@ int main(int argc, char** argv) {
 
           return {initial_pose, elbow};
         });
+
+
+    /*
+     * AE: my attempt to move the elobow to a new place
+     */
+     // First move the robot to a suitable joint configuration
+     std::array<double, 7> q_goal2 = {{0, M_PI_2, 0, 3 * M_PI_4, 0, -M_PI_2, -M_PI_4}};
+     MotionGenerator motion_generator2(0.5, q_goal2);
+
+    robot.control(motion_generator);
+
+     time = 0.0;
+     robot.control(
+         [&time, &initial_pose, &initial_elbow](const franka::RobotState& robot_state,
+                                                franka::Duration period) -> franka::CartesianPose {
+           time += period.toSec();
+
+           if (time == 0.0) {
+             initial_pose = robot_state.O_T_EE_c;
+             initial_elbow = robot_state.elbow_c;
+           }
+
+           double angle = M_PI / 10.0 * (1.0 - std::cos(M_PI / 5.0 * time));
+
+           auto elbow = initial_elbow;
+           elbow[0] += angle;
+
+           if (time >= 10.0) {
+             std::cout << std::endl << "Finished motion, shutting down example" << std::endl;
+             return franka::MotionFinished({initial_pose, elbow});
+           }
+
+           return {initial_pose, elbow};
+         });
   } catch (const franka::Exception& e) {
     std::cout << e.what() << std::endl;
     return -1;
